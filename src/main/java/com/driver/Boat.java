73c07d7736c40235835b99791e2d2f192bfbd81f
Yash69-sinha <Yashkumarlal201@gmail.com>
2024-01-22 01:06:13 +0530

    m

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -8,6 +8,22 @@ public class Boat implements WaterVehicle{
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
